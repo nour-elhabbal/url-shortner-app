a0ebad9ca5fdd4c5db61e09b735a134b73ae14bf
nour-elhabbal <nour.elhabbal9@gmail.com>
2024-07-06 20:22:04 +0300

    Fix: removed unwanted import

--- src/components/Shortener/LinksInput.tsx
+++ src/components/Shortener/LinksInput.tsx
@@ -1,4 +1,4 @@
-import { useState, useRef } from "react";
+import { useState } from "react";
 import { Button, Flex, Input, FormControl, FormErrorMessage } from "@chakra-ui/react";
 
 import { UrlsListType } from "../../types";
