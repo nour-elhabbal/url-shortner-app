730e1f6491c79ef341b63186c1016fad591e0ab6
nour-elhabbal <nour.elhabbal9@gmail.com>
2024-07-06 20:28:51 +0300

    Feat: added the shortener section to the App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,21 @@
+import { Box } from "@chakra-ui/react";
 import Boost from "./components/Boost";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
 import Hero from "./components/HeroSection";
+import Shortener from "./components/Shortener/Shortener";
 
 const App = () => {
   return (
-    <div>
+    <>
       <Header />
       <Hero />
+      <Box bgColor="#f0f1f6" pos="relative" minH="500px">
+        <Shortener />
+      </Box>
       <Boost />
       <Footer />
-    </div>
+    </>
   );
 };
 
